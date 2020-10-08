5abe0eb8a5b9be2165e34103dab4ec7d0c152d2b
Nexliath <victor.garnier@efrei.net>
2020-10-08 12:12:22 +0200

    Liaison des id panier.articles à ceux de articles

--- efrei-3a-web-TP4-master/client/components/Panier.vue
+++ efrei-3a-web-TP4-master/client/components/Panier.vue
@@ -3,15 +3,17 @@
     <h2>Mon Panier</h2>
     <article v-for="article in panier.articles" :key="article.id">
       <div class="article-content">
-        <h3>Id: {{ article.id }} - Name: {{ article.name }} - Quantity: {{ article.quantity }}</h3>
+        <h3>{{article.quantity}} {{ articles.find(a => a.id === article.id).name }} | Prix total: {{ articles.find(a => a.id === article.id).price * article.quantity }}€</h3>
+        <p>{{ articles.find(a => a.id === article.id).description }}</p>
+      
         <input
           type="number"
           v-model="articleQuantity"
-          placeholder="Quantity"
+          placeholder="Quantité"
           required
         />
         <button @click="putInPanier(article.id, articleQuantity)">
-          Change quantity
+          Changer la quantité
         </button>
       </div>
     </article>
