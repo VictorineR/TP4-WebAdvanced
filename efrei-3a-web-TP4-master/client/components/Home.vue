cc54672ba72f816f5d021ed972975d7143f9782b
Nexliath <victor.garnier@efrei.net>
2020-10-08 12:22:22 +0200

    Ajout placeholder form home.vue

--- efrei-3a-web-TP4-master/client/components/Home.vue
+++ efrei-3a-web-TP4-master/client/components/Home.vue
@@ -40,7 +40,7 @@
       <h2>Nouveau produit à ajouter</h2>
       <input type="text" v-model="newArticle.name" placeholder="Nom du produit" required>
       <input type="number" v-model="newArticle.price" placeholder="Prix" required>
-      <textarea type="text" v-model="newArticle.description" required></textarea>
+      <textarea type="text" v-model="newArticle.description" placeholder="Description du produit" required></textarea>
       <input type="text" v-model="newArticle.image" placeholder="Lien vers l'image">
       <button type="submit">Ajouter</button>
     </form>
