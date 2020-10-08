8ab5eccf1300e9d604f8bb08463fde7c463bd50d
Nexliath <victor.garnier@efrei.net>
2020-10-08 11:35:05 +0200

    Correction de bug console put

--- efrei-3a-web-TP4-master/client/components/Panier.vue
+++ efrei-3a-web-TP4-master/client/components/Panier.vue
@@ -3,11 +3,17 @@
     <h2>Mon Panier</h2>
     <article v-for="article in panier.articles" :key="article.id">
       <div class="article-content">
-        <h3>Id: {{ article.id }} - Quantity: {{ article.quantity }} </h3>
-        <input type="number" v-model="quantity" placeholder="Quantity" required/>
-        <button @click="putInPanier(article.id, quantity)">Change quantity</button>
+        <h3>Id: {{ article.id }} - Name: {{ article.name }} - Quantity: {{ article.quantity }}</h3>
+        <input
+          type="number"
+          v-model="articleQuantity"
+          placeholder="Quantity"
+          required
+        />
+        <button @click="putInPanier(article.id, articleQuantity)">
+          Change quantity
+        </button>
       </div>
-    
     </article>
   </div>
 </template>
@@ -18,21 +24,19 @@ module.exports = {
     articles: { type: Array, default: [] },
     panier: { type: Object }
   },
-  data () {
+  data() {
     return {
-    }
+      articleQuantity: ''
+    };
   },
-  async mounted () {
-  },
+  async mounted() {},
   methods: {
     putInPanier(articleId, articleQuantity) {
-      this.$emit("put-in-panier", articleId,articleQuantity);
+      this.$emit("put-in-panier", articleId, articleQuantity);
     },
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-
-
 </style>
